c9449b7a842290e017dcb4d0735dc48dbcfbfe42
Niels Avonds <niels@nobi.life>
2022-01-07 08:39:07 +0100

    Don't run tmux when running under vscode

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -2,8 +2,8 @@
 # Run tmux if exists
 if command -v tmux>/dev/null; then
 
-  # Check if we are inside TMUX
-  if [ -z $TMUX ]; then
+  # Check if we are inside TMUX or vscode
+  if [ -z $TMUX ] && ! pstree -s $$ | grep -wq code; then
 
     # Start server first. This triggers continuum restore, so we get asked about the sessions correctly
     tmux start-server
