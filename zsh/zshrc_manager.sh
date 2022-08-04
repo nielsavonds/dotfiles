d2b9e6f0e9edbe2ee006c10fee532d92f3318120
Niels Avonds <niels@nobi.life>
2022-08-04 11:03:17 +0200

    Stop pulling last configuration daily.

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -16,14 +16,4 @@ else
 	echo "tmux not installed. Run ./deploy to configure dependencies"
 fi
 
-# Pull latest configuration daily
-LASTPULL=`cat ~/dotfiles/.lastpull || echo "0"`
-SHOULDPULL=`date -d "-1 day" +%s`
-
-if [ $LASTPULL -lt $SHOULDPULL ]; then
-  echo "Updating configuration"
-  (cd ~/dotfiles && git pull && git submodule update --init --recursive)
-
-  date +%s > ~/dotfiles/.lastpull
-fi
 source ~/dotfiles/zsh/zshrc.sh
